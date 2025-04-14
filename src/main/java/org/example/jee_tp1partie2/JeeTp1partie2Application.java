20fe88bd1d6458169fd21ec0fc9d61a105b275fc
khawla <you@example.com>
2025-04-14 17:46:57 +0100

    first commit

---
+++ src/main/java/org/example/jee_tp1partie2/JeeTp1partie2Application.java
@@ -0,0 +1,13 @@
+package org.example.jee_tp1partie2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeTp1partie2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeTp1partie2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jee_tp1partie2/JeeTp1partie2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jee_tp1partie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeTp1partie2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
